9aa01d0d903fea56ed2560427273b2be6281d622
Fabiano Luiz Santos <47800473+FabianoLXS@users.noreply.github.com>
2023-01-19 15:58:26 -0300

    commit

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+
+const data = {
+  run: ["01-01", "01-02", "01-04", "01-05"],
+  salad: ["01-03"],
+  dog: ["01-02"],
+}
+
+nlwSetup.setData(data)
+nlwSetup.load()
